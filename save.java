107f7902769df3245ba21458fde024060370a4a4
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-05-29 16:04:41 +0530

    Update save.java

--- save.java
+++ save.java
@@ -1,4 +1,6 @@
 
+// // // // Input: arr[] = [4, 5, 1, 2]
+// // // // Output: [2, 1, 5, 4]
 // // // // Explanation: The first element 4 moves to last position, the second element 5 moves to second last and so on.
 // // // // */
 
