79beb84d61352eaa36005b42cfefa326a7887d3b
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-05-30 23:50:30 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,7 +1,4 @@
 
-        for(int i=0;i<ans.length;i++){
-            System.out.print(Arrays.toString(ans[i]));
-            if(i<ans.length-1) System.out.print(", ");
         }
         System.out.println("]");
     }
