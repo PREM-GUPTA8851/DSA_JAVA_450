9a5452b99d69dc4a8e11c496050ab383a87cbb78
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-05-30 23:45:50 +0530

    Comment out main class and linked list creation

--- save.java
+++ save.java
@@ -1092,3 +1092,15 @@ public class Main {
 // // //         return false;
 // // //     }
 // // // }
+
+
+
+// // // /* Main class */
+// // // public class Main {
+// // //         public static void main(String[] args) {
+
+// // //         // Creating linked list
+// // //         Node head = new Node(1);
+// // //         head.next = new Node(2);
+// // //         head.next.next = new Node(3);
+// // //         head.next.next.next = new Node(4);
