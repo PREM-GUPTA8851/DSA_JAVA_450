ba1ca5cc564b87f8383e6532091024295d595753
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-05-30 23:50:09 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,15 +1,4 @@
 
-
-        return ans.toArray(new int[ans.size()][]);
-    }
-
-    public static void main(String[] args) {
-        int[][] intervals={{1,3},{2,6},{8,10},{15,18}};
-
-        Solution obj=new Solution();
-        int[][] ans=obj.merge(intervals);
-
-        System.out.print("[");
         for(int i=0;i<ans.length;i++){
             System.out.print(Arrays.toString(ans[i]));
             if(i<ans.length-1) System.out.print(", ");
