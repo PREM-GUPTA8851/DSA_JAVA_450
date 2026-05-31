850cb65897555fbed70be07f98bbd1b93de697c5
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-05-31 23:50:58 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -35,7 +35,9 @@ class Solution {
     public int search(int[] nums, int target) {
         int low=0,high=nums.length-1;
         while(low<=high){
-        
+           
+                if(target>nums[mid] && target<=nums[high])
+                    low=mid+1;
                 else
                     high=mid-1;
             }
