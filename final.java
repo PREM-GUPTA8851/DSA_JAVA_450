cf410907d475495811a34733c5e8da02fea515ce
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-02 23:15:42 +0530

    Create final.java

---
+++ final.java
@@ -0,0 +1,6 @@
+// // // // // ================================(Array)===========================
+// Reverse the Array 
+/*
+Input: arr[] = [1, 4, 3, 2, 6, 5]  
+Output:  [5, 6, 2, 3, 4, 1]
+Explanation: The first element 1 moves to last position, the second element 4 moves to second-last and so on.
