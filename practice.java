c231ec618ceb19a1874bdebb7d651a7f11ab07b8
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-06 23:52:11 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,4 +1,23 @@
 
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals={{1,3},{2,6},{8,10},{15,18}};
+
+        Solution obj=new Solution();
+        int[][] ans=obj.merge(intervals);
+
+        System.out.print("[");
+        for(int i=0;i<ans.length;i++){
+            System.out.print(Arrays.toString(ans[i]));
+            if(i<ans.length-1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
 class Solution {
     public int search(int[] nums, int target) {
         int low=0,high=nums.length-1;
