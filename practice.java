818266c4c2c0603001d9c3400c1ce058bc1471ab
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-06 23:48:38 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,17 +1,4 @@
-import java.util.*;
 
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-
-        List<int[]> ans=new ArrayList<>();
-
-        for(int[] it:intervals){
-            if(ans.isEmpty() || ans.get(ans.size()-1)[1]<it[0]){
-                ans.add(it);
-            }else{
-                ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],it[1]);
-            }
         }
 
         return ans.toArray(new int[ans.size()][]);
