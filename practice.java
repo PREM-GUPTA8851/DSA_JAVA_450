a9e163af98cf94ca0f68e0c73423f5ef8f71416d
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-06 23:54:49 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,40 +1,4 @@
 
-            if(ans.isEmpty() || ans.get(ans.size()-1)[1]<it[0]){
-                ans.add(it);
-            }else{
-                ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],it[1]);
-            }
-        }
-
-        return ans.toArray(new int[ans.size()][]);
-    }
-
-    public static void main(String[] args) {
-        int[][] intervals={{1,3},{2,6},{8,10},{15,18}};
-
-        Solution obj=new Solution();
-        int[][] ans=obj.merge(intervals);
-
-        System.out.print("[");
-        for(int i=0;i<ans.length;i++){
-            System.out.print(Arrays.toString(ans[i]));
-            if(i<ans.length-1) System.out.print(", ");
-        }
-        System.out.println("]");
-    }
-}
-class Solution {
-    public int search(int[] nums, int target) {
-        int low=0,high=nums.length-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
-
-            if(nums[mid]==target) return mid;
-
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<nums[mid])
-                    high=mid-1;
                 else
                     low=mid+1;
             }else{
