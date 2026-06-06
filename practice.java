7ba054a3ef6e6efb990b13c28339294dba531cc9
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-06 23:51:11 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,8 +1,4 @@
 
-import java.util.*;
-
-class Solution {
-    public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
 
         List<int[]> ans=new ArrayList<>();
