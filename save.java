4fbf29a061984d3de5b48466ae1dbb79d7027c2a
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-16 21:57:21 +0530

    Update save.java

--- save.java
+++ save.java
@@ -13,6 +13,31 @@
 // // // // Explanation: The first element 4 moves to last position, the second element 5 moves to second last and so on.
 // // // // */
 
+// merge intervals
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> ans = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                ans.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        
+        ans.add(new int[]{start, end});
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
