8cc6aad4a5a8a7744be9cdb5af042a798ab28e7f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-26 12:09:42 +0530

    Update final.java

--- final.java
+++ final.java
@@ -1257,7 +1257,11 @@ class Solution {
 }
 
 // ===============================(Matrix)=======================================
-
+/*
+Input: mat[][] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+Output: [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+Explanation: 
+*/
 // 1. Spirally traverse a matrix
 class Solution {
 	public ArrayList<Integer> spirallyTraverse(int[][] mat) {
