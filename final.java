975a463710fb4ab7ae60f1d78fd4bc3bafb72144
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-27 19:55:51 +0530

    Update final.java

--- final.java
+++ final.java
@@ -1308,6 +1308,14 @@ class Solution {
 
 
 //2. Search in a 2D matrix.
+/*
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+*/
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // total elements
