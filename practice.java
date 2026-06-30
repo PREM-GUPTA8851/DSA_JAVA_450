1fcf6f5b3f355906c5ad65daf2364ecbabd848fa
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-30 19:22:50 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -123,7 +123,6 @@ class Solution {
             // price --> 7, 1, 5, 3, 6
             minPrice = Math.min(minPrice, price);
             // minPrice = min(max, 7) --> 7
-            // minPrice = min(7, 1) --> 1
             // minPrice = min(1, 5) -- > 1
             // minPrice = min(1,2) --> 1
             maxProfit = Math.max(maxProfit, price - minPrice);
