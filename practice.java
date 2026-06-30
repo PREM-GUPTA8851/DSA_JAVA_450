24ca3a2daa200ef422aa212af224b4eb4d81b146
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-30 19:03:57 +0530

    Clean up comments in practice.java
    Removed commented-out code for clarity.

--- practice.java
+++ practice.java
@@ -131,7 +131,6 @@ class Solution {
             //maxprofit = max(0, 7 - 7)--> 0
             // maxProfit = max(0, 1-1) --> 0
             // maxprofit = max(0, 5-1)--> 4
-            // maxprofit = max(4, 3-1)--> 4
         }
         return maxProfit;
         // 5
