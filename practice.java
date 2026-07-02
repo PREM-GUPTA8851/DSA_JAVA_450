06e4015f895d79dcef812e29c833d54985de2fc4
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-02 16:40:39 +0530

    Reverse an Array method

--- practice.java
+++ practice.java
@@ -1,133 +1,39 @@
-// Java Code to print duplicate characters 
-// and their counts using Hashing
-import java.util.*;
+// revision :-- 
 
-class GfG {
+import java.util.Arrays;
 
-    // Function to print duplicate characters with their count
-    public static void printDuplicates(String s) {
+class main {
+    
+    // function to reverse an array
+    static void reverseArray(int[] arr) {
+        
+        // Initialize left to the beginning
+        // and right to the end
+        int left = 0, right = arr.length - 1;
 
-        // Hash map to store frequency of each character
-        HashMap<Character, Integer> freq = new HashMap<>();
+        // Iterate till left is less than right
+        while (left < right) {
+            
+            // Swap the elements at left
+            // and right position
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
 
-        // Count frequency of each character
-        for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-        }
+            // Increment the left pointer
+            left++;
 
-        // Traverse the map and print characters with count > 1
-        for (Map.Entry<Character, Integer> it : freq.entrySet()) {
-            if (it.getValue() > 1) {
-                System.out.print("['" + it.getKey() + "', " + it.getValue() + "], ");
-            }
+            // Decrement the right pointer
+            right--;
         }
     }
 
     public static void main(String[] args) {
+        int[] arr = { 1, 4, 3, 2, 6, 5 };
 
-        String s = "geeksforgeeks";
+        reverseArray(arr);
 
-        printDuplicates(s);
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " ");
     }
 }
-
-
-class Solution {
-    public int search(int[] nums, int target) {
-        int low=0,high=nums.length-1;
-
-        while(low<=high){
-            int mid=low+(high-low)/2;
-
-            if(nums[mid]==target) return mid;
-
-            if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<nums[mid])
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }else{
-                if(target>nums[mid] && target<=nums[high])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }
-        }
-        return -1;
-    }
-}
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()) return findMedianSortedArrays(nums2,nums1);
-
-        int m=nums1.size(),n=nums2.size();
-        int low=0,high=m;
-
-        while(low<=high){
-            int cut1=(low+high)/2;
-            int cut2=(m+n+1)/2-cut1;
-
-            int l1=(cut1==0)?INT_MIN:nums1[cut1-1];
-            int l2=(cut2==0)?INT_MIN:nums2[cut2-1];
-            int r1=(cut1==m)?INT_MAX:nums1[cut1];
-            int r2=(cut2==n)?INT_MAX:nums2[cut2];
-
-            if(l1<=r2 && l2<=r1){
-                if((m+n)%2==0)
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                return max(l1,l2);
-            }
-            else if(l1>r2) high=cut1-1;
-            else low=cut1+1;
-        }
-        return 0.0;
-    }
-};
-
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()) return findMedianSortedArrays(nums2,nums1);
-
-        int m=nums1.size(),n=nums2.size();
-        int low=0,high=m;
-
-        while(low<=high){
-            int cut1=(low+high)/2;
-            int cut2=(m+n+1)/2-cut1;
-
-            int l1=(cut1==0)?INT_MIN:nums1[cut1-1];
-            int l2=(cut2==0)?INT_MIN:nums2[cut2-1];
-            int r1=(cut1==m)?INT_MAX:nums1[cut1];
-            int r2=(cut2==n)?INT_MAX:nums2[cut2];
-
-            if(l1<=r2 && l2<=r1){
-                if((m+n)%2==0)
-                    return (max(l1,l2)+min(r1,r2))/2.0;
-                return max(l1,l2);
-            }
-            else if(l1>r2) high=cut1-1;
-            else low=cut1+1;
-        }
-        return 0.0;
-    }
-};            
-class Solution {
-    public int maxProfit(int[] prices) {
-
-        int minPrice = Integer.MAX_VALUE; 
-        int maxProfit = 0;
-
-        for (int price : prices) {
-            minPrice = Math.min(minPrice, price);
-            maxProfit = Math.max(maxProfit, price - minPrice);
-        }
-        return maxProfit;
-    }
-}
-
-	
-	
-
-        
