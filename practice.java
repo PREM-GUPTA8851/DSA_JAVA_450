e13d13e9d9e1ac278cbaa1d781343e3d83a8f3f6
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-02 20:56:09 +0530

    Add Dutch National Flag for sorting 0,1,2

--- practice.java
+++ practice.java
@@ -1,5 +1,40 @@
 // revision :-- 
 
+class Solution {
+    public void sort012(int[] arr) {
+        // by using dutch national flag algorithms
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                // swap with arr[low]
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
+            }
+            else if(arr[mid]  == 1){
+                // 1 uski correct position m h
+                mid++;
+            }
+            else{
+                // arr[mid] ==2 
+                // swap with high
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {0, 1, 2, 0, 1, 2};
+        sort012(arr);
+    }
+}
+
 
 // 2.WAFP for min and max in an array .
 import java.util.ArrayList;
