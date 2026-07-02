ea2453170e76de5f21b1b79767c6b80c0c642e58
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-02 20:12:47 +0530

    update the two pointer method

--- practice.java
+++ practice.java
@@ -1,39 +1,30 @@
 // revision :-- 
 
+// WAFP for reverse an array 
 import java.util.Arrays;
 
-class main {
-    
-    // function to reverse an array
-    static void reverseArray(int[] arr) {
-        
-        // Initialize left to the beginning
-        // and right to the end
-        int left = 0, right = arr.length - 1;
+class Main{
+    static void ReverseArray(int[] arr){
+        // reverse an array by using swap 
+        int left= 0;
+        int right = arr.length -1;
 
-        // Iterate till left is less than right
-        while (left < right) {
-            
-            // Swap the elements at left
-            // and right position
+        while(left < right){
+            // swap from 
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
-
-            // Increment the left pointer
             left++;
-
-            // Decrement the right pointer
             right--;
         }
     }
+    
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
 
-    public static void main(String[] args) {
-        int[] arr = { 1, 4, 3, 2, 6, 5 };
-
-        reverseArray(arr);
-
-        for (int i = 0; i < arr.length; i++) 
-            System.out.print(arr[i] + " ");
+        ReverseArray(arr);
+        for(int i: arr){
+            System.out.printf(i + ", ");
+        }
     }
 }
