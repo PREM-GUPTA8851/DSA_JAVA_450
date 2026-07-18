06467f455171963456e9d1e107e945eb8ae28efc
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-18 18:49:37 +0530

    Two Pointers (Linear Scan)

--- final.java
+++ final.java
@@ -1865,3 +1865,29 @@ class Solution {
         return s.substring(start, start + maxLength);
     }
 }
+
+// 7.Is Subsequence
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        // i -> s ko traverse karega
+        // j -> t ko traverse karega
+        int i = 0, j = 0;
+
+        // jab tak dono strings ke andar hain
+        while (i < s.length() && j < t.length()) {
+
+            // character match hua
+            if (s.charAt(i) == t.charAt(j)) {
+                i++; // s ka next character
+            }
+
+            // t hamesha aage badhega
+            j++;
+        }
+
+        // agar s ke saare characters match ho gaye
+        return i == s.length();
+    }
+}
