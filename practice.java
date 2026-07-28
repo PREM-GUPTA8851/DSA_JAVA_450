262c5fa8461316b99a988c146cda69b6c9b583b6
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-07-28 20:47:06 +0530

    Remove union method and add negative number movement comment
    Removed the union of two arrays implementation and added a comment for a new feature.

--- practice.java
+++ practice.java
@@ -1,17 +1,4 @@
-//6. Union of 2 arrays and return in Sorted order**
-    
-class Solution {
-    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
-        // code here
-        // we use Tree_set for union of 2 arrays
-        TreeSet<Integer> ans = new TreeSet<>();
-        // now add elements in tree_set
-        for(int num: a) ans.add(num);
-        for(int num: b) ans.add(num);
-        
-        return new ArrayList<>(ans);
-    }
-}
+
 
 // 5.Java program to Move all negative numbers
 // to beginning and positive to end.
