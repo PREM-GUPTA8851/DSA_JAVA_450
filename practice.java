b7a0dd73970c5d408911526b036c658a68ac7bdc
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-06 06:53:42 +0530

    Add findPeakElement method to Solution class
    Implemented binary search to find peak element in an array.

--- practice.java
+++ practice.java
@@ -1,4 +1,63 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
 
+        int low = 0, high = nums.length - 1;
+
+        // Dry Run
+        // nums = [1,2,3,1]
+        //
+        // low = 0, high = 3
+        //
+        // ---------------- Iteration 1 ----------------
+        // mid = 0 + (3-0)/2 = 1
+        //
+        // nums[mid] = 2
+        // nums[mid+1] = 3
+        //
+        // 2 > 3 ?
+        // No
+        //
+        // Peak right side me hoga.
+        // low = mid + 1 = 2
+        //
+        // low = 2, high = 3
+        //
+        // ---------------- Iteration 2 ----------------
+        // mid = 2 + (3-2)/2 = 2
+        //
+        // nums[mid] = 3
+        // nums[mid+1] = 1
+        //
+        // 3 > 1 ?
+        // Yes
+        //
+        // Peak left side (mid included) me hoga.
+        // high = mid = 2
+        //
+        // low = 2, high = 2
+        //
+        // low < high ?
+        // 2 < 2 -> False
+        //
+        // return low = 2
+        // Peak Element = nums[2] = 3
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // Peak left side (mid included)
+                high = mid;
+            } else {
+                // Peak right side
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
 class Solution {
     
     public int search(int[] nums, int target) {
