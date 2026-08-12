8aad10b99be28f34a416b131e4407aed8f9ee28e
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-12 23:08:10 +0530

    implement Greedy Traversal / Greedy Approach.

--- final.java
+++ final.java
@@ -2068,7 +2068,7 @@ class Solution {
     }
 }
 
-14. Valid Parenthesis String
+// 14. Valid Parenthesis String
 	class Solution {
     public boolean checkValidString(String s) {
         // Dry Run
@@ -2122,5 +2122,34 @@ class Solution {
         }
 
         return low == 0;
+    }
+}
+
+// 24.Roman to Integer
+class Solution {
+    public int romanToInt(String s) {
+        int ans = 0;
+// MCMXCIV
+        for(int i = 0; i < s.length(); i++){
+            char left = s.charAt(i); 
+        // main logic is left wala chota ho right wale se to ans m subtract kr denge 
+        if(i + 1 < s.length() && value(left) < value(s.charAt(i + 1)))
+        ans -= value(left);
+        // aur agar aesa nhi h (right wala chota h left wale se to ans m add kr denge)
+        else 
+            ans += value(left);
+        }
+        return ans;
+    }
+    int value(char ch){
+            if(ch == 'I') return 1;
+            if(ch == 'V') return 5;
+            if(ch == 'X') return 10;
+            if(ch == 'L') return 50;
+            if(ch == 'C') return 100;
+            if(ch == 'D') return 500;
+            if(ch == 'M') return 1000;
+
+        return 0;
     }
 }
