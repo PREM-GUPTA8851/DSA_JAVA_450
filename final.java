583ea770cadad0cb8223d03a47b8ad52669d6fe6
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-17 23:08:32 +0530

    Add isBalanced method to check bracket balance
    Implemented a method to check if brackets are balanced using a stack.

--- final.java
+++ final.java
@@ -2125,6 +2125,44 @@ class Solution {
     }
 }
 
+// by using stack 
+class Solution {
+	public boolean isBalanced(String s) {
+		// [{()}]
+		// odd length wale to pehle hi false ho jaenge
+		Stack<Character> st = new Stack<>();
+		// logic is stack use kiya jae aur usme
+		for (int i = 0; i < s.length(); i++) {
+			// sb push kiya jae open bracket k case me
+			char current = s.charAt(i); // [
+			if (!st.isEmpty() && (
+			(st.peek() == '[' && current == ']') ||
+			(st.peek() == '{' && current == '}') ||
+			(st.peek() == '(' && current == ')'))) {
+				st.pop();
+				
+				// aur jb close bracket mila tb aapn
+				// stack k top check krenge
+				// if stack k top us similar bracket k open
+				// side mila tb stack ko pop kr denge
+			}
+			else {	// sb push kiya jae open bracket k case me
+				st.push(current); //
+			}
+		}
+		// at the end stack empty mila to fhr
+		// true return krenge
+		// otherwise false return krenge
+		if (st.isEmpty()) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+}
+
+
 // 24.Roman to Integer
 class Solution {
     public int romanToInt(String s) {
