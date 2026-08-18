d7acdae1351a1c323864d0455aa98fb7d46ca93c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-18 12:47:02 +0530

    Implement valEqualToPos method in Solution class
    Added a method to find values equal to their positions in the array.

--- final.java
+++ final.java
@@ -2241,3 +2241,18 @@ class Solution {
     return new int[] {first, last};
     }
 }
+
+// 2.Value equal to Position 
+class Solution {
+    public static ArrayList<Integer> valEqualToPos(int[] arr) {
+    // indexing 0 se start krenge aur fhr 
+    ArrayList<Integer> ans = new ArrayList<>();
+    for(int i = 0; i < arr.length; i++){
+    // jo number index + 1 k equal hoga usko arraylist m
+    if(i + 1 == arr[i]) ans.add(arr[i]);
+    // add krte jaenge 
+    }
+    return ans;
+    }
+}
+
