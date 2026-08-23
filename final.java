dc979a14be49adb502b117e2c52bdd237c858c95
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-24 00:07:39 +0530

    Add pivot index method to Solution class
    Implemented pivot index calculation with left and right sums.

--- final.java
+++ final.java
@@ -2733,3 +2733,85 @@ class Solution {
         // b = [5,7,9]
     }
 }
+
+// 21.find pivot index
+
+/*
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ 
+*/
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int n = nums.length;
+
+        int[] lsum = new int[n];
+        // left sum store karne ke liye
+
+        int[] rsum = new int[n];
+        // right sum store karne ke liye
+
+
+        // Left Sum
+        for (int i = 1; i < n; i++) {
+            lsum[i] = lsum[i - 1] + nums[i - 1];
+        }
+        /*
+        nums = [1,7,3,6,5,6]
+
+        lsum[1] = 0 + 1 = 1
+        lsum[2] = 1 + 7 = 8
+        lsum[3] = 8 + 3 = 11
+
+        lsum = [0,1,8,11,17,22]
+        */
+
+
+        // Right Sum
+        for (int i = n - 2; i >= 0; i--) {
+            rsum[i] = rsum[i + 1] + nums[i + 1];
+        }
+        /*
+        rsum[4] = 0 + 6 = 6
+        rsum[3] = 6 + 5 = 11
+
+        rsum = [27,20,17,11,6,0]
+        */
+
+
+        // dono sum compare karo
+        for (int i = 0; i < n; i++) {
+
+            if (lsum[i] == rsum[i]) {
+                return i;
+                // i = 3
+                // leftSum = 11
+                // rightSum = 11
+            }
+        }
+
+        return -1;
+    }
+}
