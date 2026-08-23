58b5acf5adbdaf45f5af92d1c44ecc7939df35af
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-08-23 16:23:30 +0530

    Add method to find the middle of three numbers

--- final.java
+++ final.java
@@ -2427,3 +2427,15 @@ class Solution {
         // perfect squares = 1, 4, 9
     }
 }
+
+// 5. Middle of Three 
+class Solution {
+    int middle(int a, int b, int c) {
+        // code here
+        // agar a middle h
+        if((a > b && a < c) || (a > c && a < b)) return a;
+        else if((b > a && b < c) || (b > c && b < a)) 
+        return b;
+        return c;
+    }
+}
